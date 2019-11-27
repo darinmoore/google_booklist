cb40ff1e01671f94a52cb2e1afd0af0508f372f6
dsmoore <dsmoore@ucsd.edu>
2019-11-27 13:26:05 -0800

    Created mock queries so there are less calls made to google books api when testing

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from google_booklist import *
+from saved_queries import *
 
 TEST_BOOKLIST = 'my_test_booklist.txt'
 
@@ -17,38 +18,34 @@ class TestQuery:
         assert not result
 
     # Test that query result is composed of multiple books
-    def test_multiple_items(self):
+    def test_multiple_items_returned(self):
         result = query('A Song of Ice and Fire')
         assert len(result['items']) > 1
 
     # Test that query result is reasonable
-    def test_query_title(self):
+    def test_query_result_has_title(self):
         title = query('isbn 9780765377135')['items'][0]['volumeInfo']['title']
         assert title == 'Mistborn'
 
 # Tests parse_json() function
 class TestParseJSON:
-    # Test that parse results in 5 or less books
-    def test_len_books(self):
-        books = parse_json(query('isbn 9780765377135'))
+    def test_parse_returns_correct_length(self):
+        books = parse_json(mistborn_query)
         assert len(books) <= 5
 
-    # Tests that parse formats book info correctly
-    def test_book_format(self):
-        books = parse_json(query('isbn 9780765377135'))
+    def test_parse_returns_correct_book_format(self):
+        books = parse_json(mistborn_query)
         assert (str(books[0]) == 
                 'Title: Mistborn\nAuthors: Brandon Sanderson\nPublisher: Tor Teen\n\n')
         
-    # Tests that parse formats book info correctly with multiple authors
-    def test_multiple_authors(self):
-        books = parse_json(query('isbn 9780765377135'))
+    def test_multiple_authors_parse_correctly(self):
+        books = parse_json(mistborn_query)
         assert (str(books[3]) == 
                 "Title: Missy Piggle-Wiggle and the Won't-Walk-the-Dog Cure\n" +
                 "Authors: Ann M. Martin, Annie Parnell\nPublisher: Missy Piggle-Wiggle\n\n")
 
-    # Tests that unknown filled in when a field is missing
-    def test_unknown_authors(self):
-        result = query("Lord of the Rings")
+    def test_parse_with_empty_field(self):
+        result = lotr_query
         del result['items'][0]['volumeInfo']['authors']
         books = parse_json(result)
         assert (str(books[0]) == 
@@ -57,19 +54,16 @@ class TestParseJSON:
 
 # Tests add_book_to_list() function
 class TestAddBook:
-    # NOTE: These tests require that no booklist is present before running
-    # Tests that book is added to book list
-    def test_add_book(self):
-        books = parse_json(query('isbn 9780765377135'))
+    def test_add_book_to_booklist(self):
+        books = parse_json(mistborn_query)
         add_book_to_list(books[0], booklist=TEST_BOOKLIST)
         with open(TEST_BOOKLIST, 'r') as f:
             lines = f.readlines()
         assert (''.join(lines) == 
                 'Title: Mistborn\nAuthors: Brandon Sanderson\nPublisher: Tor Teen\n\n')
 
-    # Tests that book is appended to list
-    def test_append_book(self):
-        books = parse_json(query("Barbarian Days"))
+    def test_append_book_to_end_of_booklist(self):
+        books = parse_json(barbarian_query)
         add_book_to_list(books[0], booklist=TEST_BOOKLIST)
         with open(TEST_BOOKLIST, 'r') as f:
             lines = f.readlines()
